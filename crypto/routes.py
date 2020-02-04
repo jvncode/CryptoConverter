e67ad83ec48c5063a44a35ca76a93932010cb677
JVNCODE <jvn@MacBook-Pro-de-JESUS.local>
2020-02-04 01:05:12 +0100

    Conexión con BBDD y reestructuración de templates

--- crypto/routes.py
+++ crypto/routes.py
@@ -1,17 +1,49 @@
 from crypto import app
 from flask import render_template, request, redirect, url_for
 from crypto.forms import PurchaseForm
+import datetime
+import sqlite3
+
+BBDD = './data/CryptoData.db'
 
 cryptos = {"EUR":1, "BTC":3.5, "ETH":4.8, "XRP":3.76, "LTC":6.4, "BCH":1.5, "BNB":76.4, "USDT":2.6, "EOS":4.3, "BSV":1.41, "XLM":2.43, "ADA":3.6, "TRX":6.8}
+dt = datetime.datetime.now()
+
+
+
+def dataQuery(consulta):
+
+    conex = sqlite3.connect(BBDD)
+    cursor = conex.cursor()
+
+    movs = cursor.execute(consulta).fetchall()
+
+    if len(movs) == 1:
+        movs = movs[0]
+    elif len(movs) == 0:
+        movs = None
+
+    conex.commit()
+    conex.close()
+
+    return movs
+
 
 @app.route("/")
 def index():
+    '''
     slctFrom=None
     slctTo=None
     units=0
     quant = 0
     pu = 0
-    return render_template("index.html", data=[quant, pu, units, slctFrom, slctTo])
+    fecha=dt.strftime("%d/%m/%Y")
+    hora=dt.strftime("%H:%M:%S")
+    '''
+
+    registros = dataQuery("SELECT date, time, from_currency, from_quantity, to_currency, to_quantity FROM MOVEMENTS;")
+
+    return render_template("index.html", registros = registros)
 
 
 @app.route("/purchase", methods=['GET', 'POST'])
@@ -22,6 +54,7 @@ def purchase():
     units=request.values.get("inputCantidad")
     quant = 0
     pu = 0
+    
     if slctFrom and slctTo != '':
         quant=(float(units) * float(cryptos[slctTo])) / float(cryptos[slctFrom])
         pu = float(cryptos[slctTo]) / float(cryptos[slctFrom])
@@ -36,8 +69,21 @@ def purchase():
         return render_template("purchase.html", form=form, data=[quant,pu, slctFrom])
 
     if request.values.get("submitCompra"):
+        fecha=dt.strftime("%d/%m/%Y")
+        hora=dt.strftime("%H:%M:%S")
 
-        return render_template("index.html", form=form, data=[quant, pu, int(units), slctFrom, slctTo])
+        conex = sqlite3.connect(BBDD)
+        cursor = conex.cursor()
+        mov = "INSERT INTO MOVEMENTS(date, time, from_currency, from_quantity, to_currency, to_quantity) VALUES(?, ?, ?, ?, ?, ?);"
+
+        cursor.execute(mov, (fecha, hora, slctFrom, quant, slctTo, units))
+
+        conex.commit()
+        registros = dataQuery("SELECT date, time, from_currency, from_quantity, to_currency, to_quantity FROM MOVEMENTS;")
+
+        conex.close()
+        
+        return render_template("index.html", form=form, registros=registros)
 
 
 @app.route("/status")
